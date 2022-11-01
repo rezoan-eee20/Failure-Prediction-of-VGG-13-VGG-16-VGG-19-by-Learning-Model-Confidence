51328d5b79317a9f09838a10e53c4a694416391a
sakibmohammad <60448661+sakibmohammad@users.noreply.github.com>
2022-11-01 17:07:08 -0500

    Update vgg16_init.py

--- vgg16_init.py
+++ vgg16_init.py
@@ -6,36 +6,83 @@ from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
 
-# enable GPU
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-    
-print('Using PyTorch version:', torch.__version__, ' Device:', device)
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Load the CIFAR-100 training data
-train_loader = torch.utils.data.DataLoader(
-datasets.CIFAR100('data', train=True, download=True,
-transform=transforms.Compose([
-transforms.ToTensor(),
-transforms.Normalize((0.1307,), (0.3081,))
-])),
-batch_size=64, shuffle=True)
+def data_loader(data_dir,
+                batch_size,
+                random_seed=42,
+                valid_size=0.1,
+                shuffle=True,
+                test=False):
+  
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+
+    # define transforms
+    transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            normalize,
+    ])
+
+    if test:
+        dataset = datasets.CIFAR100(
+          root=data_dir, train=False,
+          download=True, transform=transform,
+        )
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=shuffle
+        )
+
+        return data_loader
+
+    # load the dataset
+    train_dataset = datasets.CIFAR100(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    valid_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler)
+ 
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler)
+
+    return (train_loader, valid_loader)
 
 
-# Load the CIFAR-100 test data
-test_loader = torch.utils.data.DataLoader(
-datasets.CIFAR100('data', train=False, transform=transforms.Compose([
-transforms.ToTensor(),
-transforms.Normalize((0.1307,), (0.3081,))
-])),
-batch_size=64, shuffle=True)
+# CIFAR100 dataset 
+train_loader, valid_loader = data_loader(data_dir='./data',
+                                         batch_size=64)
 
-#VGG16_architecture
+test_loader = data_loader(data_dir='./data',
+                              batch_size=64,
+                              test=True)
+
 
 class VGG16(nn.Module):
-    def __init__(self, num_classes=100):
+    def __init__(self, num_classes):
         super(VGG16, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
@@ -96,7 +143,7 @@ class VGG16(nn.Module):
             nn.MaxPool2d(kernel_size = 2, stride = 2))
         self.fc = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(512, 4096),
+            nn.Linear(7*7*512, 4096),
             nn.ReLU())
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
@@ -126,7 +173,8 @@ class VGG16(nn.Module):
         return out
 
 num_classes = 100
-num_epochs = 200
+num_epochs = 500
+batch_size = 64
 learning_rate = 0.001
 
 model = VGG16(num_classes).to(device)
@@ -138,9 +186,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
 # Train the model
-total_step = len(train_loader)      
+total_step = len(train_loader)
 
-import gc
+
 total_step = len(train_loader)
 
 for epoch in range(num_epochs):
@@ -157,35 +205,52 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        del images, labels, outputs
-        torch.cuda.empty_cache()
-        gc.collect()
 
-    print ('Epoch [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, loss.item()))
+    print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
             
+    # Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+    
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total)) 
 
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        del images, labels, outputs
+    #testing
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
 
-    print('Accuracy of the network on the {} test images: {} %'.format(10000, 100 * correct / total))
+        print('Accuracy of the network on the {} test images: {} %'.format(10000, 100 * correct / total))   
+
+# Save the model
+torch.save(model.state_dict(), "mnist_mlp.pt")
+
+#conf_matrix and per_class_accuracy
 
 from sklearn.metrics import confusion_matrix
 
 #nb_classes = 10
 
 # Initialize the prediction and label lists(tensors)
-predlist=torch.zeros(0,dtype=torch.long, device=device)
-lbllist=torch.zeros(0,dtype=torch.long, device=device)
+predlist=torch.zeros(0,dtype=torch.long, device='cpu')
+lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
 
 with torch.no_grad():
     for i, (inputs, classes) in enumerate(test_loader):
@@ -205,4 +270,4 @@ print(conf_mat)
 
 #Per-class accuracy
 class_accuracy=100*conf_mat.diagonal()/conf_mat.sum(1)
-print(class_accuracy)
+print(class_accuracy)        
